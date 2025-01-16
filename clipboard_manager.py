61525445fc0ae7bbab0dc2e84334bab23834d309
Shahid Alam <shahid.alam57@gmail.com>
2025-01-16 22:02:18 +0530

    added fujnction for load and list

--- clipboard_manager.py
+++ clipboard_manager.py
@@ -19,16 +19,15 @@ def load_items(filepath):
     try:
         with open(filepath,"r") as f:
             data = json.load(f)
-            print(data)
             return data
     #we return an empty object
     except:
         return {}
 
 #  selects the arguments after 'python clipboard_manager.py'.
-print(sys.argv[1:])
+# print(sys.argv[1:])
 
-save_items("test.json",{"hell": "cute"})
+# save_items("test.json",{"hell": "cute"})
 
 
 if len(sys.argv) ==2:
@@ -38,19 +37,29 @@ if len(sys.argv) ==2:
     if command == "save":
         #get the input as key to ref the data being pasted
         key = input("enter a key label for the data: ").strip()
+
+        #validation for space
         if key == "":
             print("please enter a key without spaces")
         elif " " in key:
             print("please do not include spaces in commands")
         else:     
+
             #as soon as we enter the key this will get the latest content from sys clipboard
             data[key] = clipboard.paste()
             #save the data to the loaded file
             save_items(SAVED_DATA,data)
+            print("content stored successfully")
     elif command == "load":
-        pass
+        key = input("please enter the key for the data: ")
+        if key in data:
+            #if the key is present in the clipboard json, copy it to the clipboard of the system
+            clipboard.copy(data[key])
+            print("data copied to your clipboard successfully!")
+        else:
+            print("key not found")
     elif command == "list":
-        pass
+        print(data)
     else:
         print("unknown command")
 else:
