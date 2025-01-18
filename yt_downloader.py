222c9f3a131d0dcf58c83f47f5e6ee26f5e30bf1
Shahid Alam <shahid.alam57@gmail.com>
2025-01-18 14:19:45 +0530

    added more code to yt dfownloader

--- yt_downloader.py
+++ yt_downloader.py
@@ -2,6 +2,8 @@ from pytubefix import YouTube
 import tkinter as tk
 from tkinter import filedialog
 
+
+
 url = "https://www.youtube.com/watch?v=zT7niRUOs9o&t=967s"
 
 path = "."
@@ -9,9 +11,10 @@ path = "."
 def download_video(url,save_path):
     try:
         yt = YouTube(url)
-        streams = yt.streams.filter(progressive=True, file_extension="mp4")
-        streams.get_highest_resolution().download(save_path)
-        print(f"video downloaded to {save_path}")
+        streams = yt.streams.filter(progressive=False, file_extension="mp4")
+        print(streams)
+        # streams.get_highest_resolution().download(save_path)
+        # print(f"video downloaded to {save_path}")
     except Exception as e:
         print(e)
 
